cb85c852e4f0af6061116c96c40d996bf6feae37
Samuel <80938657+SamuelKinoshita@users.noreply.github.com>
2022-12-17 14:52:59 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/pages/home.html")
+})
+
+router.get("/contato",(req,res)=>{
+    res.sendFile(__dirname + "/pages/contato.html")
+})
+
+app.use(router)
+
+app.listen(3000, ()=> {
+    console.log("servidor estar rodando")
+})
+
+//console.log()
+
